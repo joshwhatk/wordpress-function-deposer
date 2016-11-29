a666ccfcccd802a4d85ced1a2e5e5546d9dc67e2
Joshua Friend <joshwhatk@gmail.com>
2016-11-29 10:52:59 -0600

    Initial setup of functionality

---
+++ src/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+if(! function_exists('get_stylesheet_directory_uri')) {
+  function get_stylesheet_directory_uri()
+  {
+    return '';
+  }
+}
+
+if(! function_exists('get_template_directory_uri')) {
+  function get_template_directory_uri()
+  {
+    return '';
+  }
+}
